d7af6a0d55d7357f35d3b7e2853e85ae6896fd9b
Santiago Morales <s.moralesvranic@gmail.com>
2025-04-23 17:53:00 -0300

    Initial structure

---
+++ target/debug/deps/game_of_life-661494c24d651a3c.d
@@ -0,0 +1,5 @@
+/Users/santiagomorales/Desktop/rust_learning/rust_projects/game-of-life/target/debug/deps/libgame_of_life-661494c24d651a3c.rmeta: src/main.rs
+
+/Users/santiagomorales/Desktop/rust_learning/rust_projects/game-of-life/target/debug/deps/game_of_life-661494c24d651a3c.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/game_of_life-728d7887ed831387.d
@@ -0,0 +1,5 @@
+/Users/santiagomorales/Desktop/rust_learning/rust_projects/game-of-life/target/debug/deps/libgame_of_life-728d7887ed831387.rmeta: src/main.rs
+
+/Users/santiagomorales/Desktop/rust_learning/rust_projects/game-of-life/target/debug/deps/game_of_life-728d7887ed831387.d: src/main.rs
+
+src/main.rs:
